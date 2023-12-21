3718bc213548cc0ee464be8a6edf206372fecb9b
Ime Essien <imessien@gmail.com>
2023-12-21 00:30:28 -0500

    Update preprocess.py

--- util/preprocess.py
+++ util/preprocess.py
@@ -4,27 +4,31 @@ Contain some omics data preprocess functions
 import pandas as pd
 
 
-def separate_B(B_df_single):
+# CODAI
+
+def preprocess_data(df):
     """
-    Separate the DNA methylation dataframe into subsets according to their targeting chromosomes
+    Preprocess the omics data by standardizing the whole dataset and handling categorical columns
 
     Parameters:
-        B_df_single(DataFrame) -- a dataframe that contains the single DNA methylation matrix
+        df(DataFrame) -- a dataframe that contains the omics data
 
     Return:
-        B_df_list(list) -- a list with 23 subset dataframe
-        B_dim(list) -- the dims of each chromosome
+        df(DataFrame) -- a preprocessed dataframe
     """
-    anno = pd.read_csv('./anno/B_anno.csv', dtype={'CHR': str}, index_col=0)
-    anno_contain = anno.loc[B_df_single.index, :]
-    print('Separating B.tsv according the targeting chromosome...')
-    B_df_list, B_dim_list = [], []
-    ch_id = list(range(1, 23))
-    ch_id.append('X')
-    for ch in ch_id:
-        ch_index = anno_contain[anno_contain.CHR == str(ch)].index
-        ch_df = B_df_single.loc[ch_index, :]
-        B_df_list.append(ch_df)
-        B_dim_list.append(len(ch_df))
+    # Separate categorical columns
+    cat_cols = df.select_dtypes(include=['object']).columns.tolist()
+    num_cols = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+    cat_df = df[cat_cols]
+    num_df = df[num_cols]
 
-    return B_df_list, B_dim_list
+    # One-hot encode categorical columns
+    cat_df = pd.get_dummies(cat_df, drop_first=True)
+
+    # Standardize numerical columns
+    num_df = (num_df - num_df.mean()) / num_df.std()
+
+    # Combine categorical and numerical dataframes
+    df = pd.concat([cat_df, num_df], axis=1)
+
+    return df
